1f78e1687cada35659824e6aae67cc0763579877
Artem Golovinsky <artemgolovinsky@gmail.com>
2012-06-26 03:01:55 +0400

    seems works

--- apps/epatom/src/epatom.erl
+++ apps/epatom/src/epatom.erl
@@ -1,277 +1,158 @@
 -module(epatom).
 
--export([parse/1,parse/2]).
+-export([parse/1, parse/2, from_file/1, from_file/2]).
 
+-define(ATOM_XMLNS,"http://www.w3.org/2005/Atom"). 
+-define(FORMAT, <<"%Y-%m-%dT%H:%M:%S">>).
 
--record(fe_tree, { 
-	  xmlns,
-	  author=[],
-	  category=[],
-	  contributor=[],
-	  icon,
-	  logo,
-	  link,
-	  generator=[],
-	  id,
-	  rights,
-	  title,
-	  subtitle,
-	  updated,
-	  entry=[], 
-	  content,
-	  published,
-	  source }).
-	  
+-record(state, {filter      = undefined,
+		skip_entry  = false,
+		stack       = []}).
+		
 
--record(state, { feed_rec,
-		 temp_fe,
-		 stop_date, 
-		 prepare_elem,
-		 ready_elem}).
+from_file(Fname) ->
+    from_file(Fname, undefined).
+
+from_file(Fname, Updated) ->
+    case file:read_file(Fname) of
+	{ok, Content} ->
+	    parse(Content, Updated);
+	Error ->
+	    Error
+    end.
 
 parse(Xml) ->
-    parse2(Xml, undefined).
+    parse(Xml, undefined).
 
-parse(Xml, StrDate) ->
-    parse2(Xml, StrDate).
+parse(Xml, Filter) ->
+    parse2(Xml, Filter).
 
-parse2(Xml, StrDate)->
-    State = {ok, #state{feed_rec=undefined,
-			temp_fe=#fe_tree{},
-			stop_date=StrDate,
-			prepare_elem=undefined,	
-			ready_elem=[]
-		       }},
-    case erlsom:parse_sax(Xml, State, fun do_parse/2) of
-	{ok, #state{feed_rec=Feed}} ->
-	    {ok, Feed};
-	NotUpdated ->
-	    NotUpdated
+parse2(Xml, Filter)->
+    State = #state{filter      = Filter,
+		   skip_entry  = false,
+		   stack       = []},
+    try erlsom:parse_sax(Xml,State, fun do_parse/2) of
+	{ok, #state{stack=Stack}, _} -> {ok, Stack};
+	Other -> Other
+    catch 
+	_:Reason -> {error, Reason}
     end.
 
 
-do_parse(startDocument, Acc) ->
-    Acc;
-do_parse(endDocument, Acc) ->
-    Acc;
+do_parse({startElement,Ns,"feed",_,_Attrs},_State) when Ns /= ?ATOM_XMLNS -> 
+    throw(not_atom);
 do_parse({startElement,_, Element,_,Attrs},State) ->    
-    make_start(Element, make_attrs(Attrs), State);
-
+    make_start(to_atom(Element), make_attrs(Attrs), State);
 do_parse({endElement,_, Element,_},State) ->    
-    make_end(Element, State);
-
+    make_end(to_atom(Element), State);
 do_parse({characters, Ch}, State)->
-    make_char(Ch, State).
+    make_text(Ch, State);
+do_parse(_, State)->
+    State.
 
-
-
-
-%%% ------------------- FOR FEED ---------------------------
-make_start("feed",Attrs, {ok, #state{temp_fe = Feed} = State}) -> 
-    NewFeed = Feed#fe_tree{xmlns=gv("xmlns", Attrs)},
-    {ok, State#state{temp_fe=NewFeed}};
-
-make_start("author", _, {ok, State}) ->
-    {ok, State};
-
-make_start("name", _, {ok,State}) ->
-    {ok, State#state{prepare_elem=name}};
-
-make_start("uri", _, {ok, State}) ->
-    {ok, State#state{prepare_elem=uri}};
-
-make_start("email", _, {ok, State}) ->
-    {ok, State#state{prepare_elem=email}};
-
-make_start("category", Attr, {ok, #state{temp_fe=FeTree} = State}) ->
-    NewAttrs = [{category_attr(AttrName),Value} || {AttrName, Value} <- make_attrs(Attr)],
-    OldAttrs = FeTree#fe_tree.category, 
-    NewFeed    = FeTree#fe_tree{category=[NewAttrs, OldAttrs]},
-    {ok, State#state{temp_fe=NewFeed}};
-
-make_start("contributor", _, {ok,  State}) ->
-    {ok, State};
-
-make_start("icon", _, {ok,  State}) ->
-    {ok, State#state{prepare_elem=icon}}; 
- 
-make_start("generator", Attr, {ok, #state{temp_fe=FeTree} = State}) ->
-    NewAttrs = [{generator_attr(AttrName),Value} || {AttrName, Value} <- make_attrs(Attr)], 
-    NewFeed    = FeTree#fe_tree{generator=NewAttrs},
-    {ok, State#state{temp_fe=NewFeed, prepare_elem=generator_text}};
-
-make_start("id", _, {ok, State}) ->
-    {ok, State#state{prepare_elem=id}};
-
-make_start("link", Attr, {ok, #state{temp_fe=FeTree} = State}) ->
-    NewAttrs = [{link_attr(AttrName),Value} || {AttrName, Value} <- make_attrs(Attr)],
-    OldLink = FeTree#fe_tree.link,
-    NewFeed  = FeTree#fe_tree{link=[NewAttrs,OldLink]},
-    {ok, State#state{temp_fe=NewFeed}};
-
-make_start("logo", _, {ok, State}) ->
-    {ok, State#state{prepare_elem=logo}};
-
-make_start("rights", Attr, {ok, #state{temp_fe=FeTree}  = State}) ->
-    {PrElem, TempValue} = is_xhtml_supported(Attr),
-    NewFeed = FeTree#fe_tree{rights=TempValue},
-    {ok, State#state{temp_fe=NewFeed, prepare_elem=PrElem}};
-
-make_start("subtitle", Attr, {ok, #state{temp_fe=Feed} = State}) ->
-    {PrElem, TempValue} = is_xhtml_supported(Attr),
-    NewFeed = Feed#fe_tree{subtitle=TempValue},
-    {ok, State#state{temp_fe=NewFeed, prepare_elem=PrElem}};
-
- 
-make_start("title", Attr, {ok, #state{temp_fe=Feed} = State}) ->
-    {PrElem, TempValue} = is_xhtml_supported(Attr),
-    NewFeed = Feed#fe_tree{title=TempValue},
-    {ok, State#state{temp_fe=NewFeed, prepare_elem=PrElem}};
-
-make_start("updated", _, {ok, #state{temp_fe=_Feed} = State}) ->
-    {ok, State#state{prepare_elem=updated}};
-
-make_start("entry", _, {ok, #state{feed_rec=undefined,temp_fe=Feed} = State}) ->
-    {ok, State#state{feed_rec=Feed,temp_fe=#fe_tree{}}};
-
-make_start(_,_,State) ->
+make_start(_Name, _Attrs, #state{skip_entry=true} = State) ->
+    State;
+make_start(Name, Attrs, #state{stack=Stack} = State) ->
+    State#state{stack=[{Name, Attrs,[], undefined}|Stack]};
+make_start(_Name, _Attrs, State) ->
     State.
 
 
-
-
-
-
-
-
-make_end("author", {ok, #state{ready_elem=ReElem,temp_fe=Feed} = State}) ->
-    NewAuthor = [ReElem,Feed#fe_tree.author],
-    NewFeed = Feed#fe_tree{author=NewAuthor},
-    NewState = State#state{temp_fe=NewFeed,prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState};  
-
-make_end("contributor", {ok, #state{ready_elem=ReElem,temp_fe=Feed} = State}) ->
-    NewContributor = [ReElem,Feed#fe_tree.contributor],
-    NewFeed = Feed#fe_tree{contributor=NewContributor},
-    NewState = State#state{temp_fe=NewFeed, prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState};    
-
-make_end("icon",  {ok, #state{ready_elem=ReElem,temp_fe=Feed} = State}) ->
-    [{icon, Icon}|_] = ReElem,
-    NewFeed = Feed#fe_tree{icon=Icon},
-    NewState = State#state{temp_fe=NewFeed, prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState};   
-
-make_end("generator", {ok, #state{ready_elem=ReElem,temp_fe=Feed} = State}) ->
-    [{generator_text, Gener}|_] = ReElem,
-    AttrGen = Feed#fe_tree.generator,
-    NewFeed = Feed#fe_tree{generator=[{text,Gener}|AttrGen]},
-    NewState = State#state{temp_fe=NewFeed, prepare_elem=undefined,ready_elem=[]},
-    {ok, NewState}; 
-
-make_end("id", {ok, #state{ready_elem=ReElem,temp_fe=Feed} = State}) ->
-    [{id, Id}|_] = ReElem,
-    NewFeed = Feed#fe_tree{id=Id},
-    NewState = State#state{temp_fe=NewFeed,prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState};  
-
-make_end("logo", {ok, #state{ready_elem=ReElem,temp_fe=Feed} = State}) -> 
-    [{logo, Logo}|_] = ReElem,
-    NewFeed = Feed#fe_tree{logo=Logo},
-    NewState = State#state{temp_fe=NewFeed, prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState};    
- 
-make_end("rights", {ok, #state{ready_elem=[T|_],temp_fe=Feed} = State}) ->
-    NewFeed = Feed#fe_tree{rights=T},
-    NewState = State#state{temp_fe=NewFeed, prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState}; 
-
-make_end("subtitle", {ok, #state{ready_elem=[T|_],temp_fe=Feed} = State}) ->
-    NewFeed = Feed#fe_tree{subtitle=T},
-    NewState = State#state{temp_fe=NewFeed, prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState}; 
-
-make_end("title", {ok, #state{ready_elem=[T|_],temp_fe=Feed} = State}) ->
-    NewFeed = Feed#fe_tree{title=T},
-    NewState = State#state{temp_fe=NewFeed, prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState}; 
-
-make_end("updated", {ok, #state{ready_elem=[{updated, Updated}|_], feed_rec=undefined, stop_date=Updated }= _State}) ->  
-    {ok,not_updated};
-
-make_end("updated", {ok, #state{ready_elem=ReElem,temp_fe=Feed} = State}) ->
-    [{updated, Updated}|_] = ReElem,
-    NewFeed = Feed#fe_tree{updated=Updated},
-    NewState = State#state{temp_fe=NewFeed, prepare_elem=undefined, ready_elem=[]},
-    {ok, NewState};     
- 
-make_end("entry", {ok, #state{feed_rec=Feed,temp_fe=Entr} = State}) ->
-    OldEntr = Feed#fe_tree.entry,
-    NewFeed = Feed#fe_tree{entry=[Entr|OldEntr]},
-    {ok, State#state{feed_rec=NewFeed,temp_fe=#fe_tree{}}};
-
-make_end(_,State) ->
+make_end(entry, #state{skip_entry=true} = State) ->
+    State#state{skip_entry=false};
+make_end(ElName,#state{stack=[{ElName, Attrs, Childs, Value}]} = State) ->
+    State#state{stack=[{ElName, Attrs, lists:reverse(Childs), Value}]};
+make_end(ElName, #state{stack=[{ElName, Attrs, Childs, Value}|Stack]} = State) ->
+    [{ElName1, Attrs1, Childs1, Value1} | TStack] = Stack,
+    State#state{stack=[{ElName1, Attrs1, [{ElName, Attrs, lists:reverse(Childs), Value}|Childs1], Value1}|TStack]};
+make_end(_, State) ->
     State.
-    
 
-
-
-
-
-
-
-
-make_char(Ch, {ok, #state{prepare_elem=PrElem, ready_elem=ReElem} = State}) when PrElem /= undefined ->
-    NewState = State#state{prepare_elem=undefined,
-			   ready_elem=[{PrElem,Ch}|ReElem]},
-    {ok, NewState}; 
-make_char(_, State) ->
-    {ok, State}.
+make_text(_Text, #state{skip_entry=true} = State) ->
+    State;
+make_text(Text, #state{filter={Param, Criteria},
+		       stack=[{Param, Attrs, Childs,_},
+			      {entry, Attrs1, Childs1, Value1}|TStack]} = State) when Param == updated;
+										      Param == published ->
+   case is_date_more(Text, Criteria) of
+       false -> State#state{skip_entry=true, stack=TStack};
+       true  -> State#state{stack=[{Param, Attrs, Childs, Text}|[{entry, Attrs1, Childs1, Value1}|TStack]]}
+   end;	   
+make_text(Text, #state{stack=[{ElName, Attrs, Childs, _Value}|TStack]} = State) ->
+    State#state{stack=[{ElName, Attrs, Childs, Text}|TStack]}.
 
 make_attrs(AttrList) ->
-    [{Attr, Value} || {attribute,Attr,[],[],Value} <- AttrList].
+    [{to_atom(Attr), Value} || {attribute,Attr,_,_,Value} <- AttrList].
 
-gv(Attr, AttrList) ->
-    proplists:get_value(Attr, AttrList, undefined).
+
+
+to_atom("feed") ->        feed;
+to_atom("author") ->      author;
+to_atom("category") ->    category;
+to_atom("contributor") -> contributor;
+to_atom("generator") ->   generator;
+to_atom("icon") ->        icon;
+to_atom("id") ->          id;
+to_atom("link") ->        link;
+to_atom("logo") ->        logo;
+to_atom("rights") ->      rights;
+to_atom("subtitle") ->    subtitle;
+to_atom("title") ->       title;
+to_atom("updated") ->     updated;
+to_atom("published") ->   published;
+to_atom("rel") ->         rel;
+to_atom("content") ->     content;
+to_atom("source") ->      source;
+to_atom("summary") ->     summary;
+to_atom("entry") ->       entry;
+to_atom("type") ->        type;
+to_atom("src") ->         src;
+to_atom("term") ->        term;
+to_atom("scheme") ->      scheme;
+to_atom("label") ->       label;
+to_atom("uri") ->         uri;
+to_atom("version") ->     version;
+to_atom("href") ->        href;
+to_atom("hreflang") ->    hreflang;
+to_atom("length") ->      length;
+to_atom("name") ->        name;
+to_atom("email") ->       email;
+to_atom("height") ->      height;
+to_atom("width")  ->      widht;
+to_atom("image") ->       image;
+to_atom(String) ->        String.
     
-category_attr("term") ->
-    term;
-category_attr("scheme") ->
-    scheme;
-category_attr("label") ->
-    label;
-category_attr(_) ->
-    undefined.
-
-generator_attr("uri") ->
-    uri;
-generator_attr("version") ->
-    version.
-
-link_attr("href") ->
-    href;
-link_attr("rel") ->
-    rel;
-link_attr("type") ->
-    type;
-link_attr("hreflang") ->
-    hreflang;
-link_attr("title") ->
-    title;
-link_attr("length") ->
-    length.
-
-
-is_xhtml_supported(Attr) ->
-    case gv("type", Attr) of
-	"text" ->
-	    {text,undefined};
-	"html" ->
-	    {html,undefined};
-	_ ->
-	    {undefined, {xhtml,"not_supported"}}
+    
+is_date_more(FirstDate, SecondDate) ->
+    FirstDateT  = to_erl_date(FirstDate),
+    SecondDateT = to_erl_date(SecondDate),
+    case calendar:time_difference(FirstDateT, SecondDateT) of
+	{D, _} when D =< 0 -> true;
+	_ -> false
     end.
-    		     
+
+to_erl_date(Date) when is_tuple(Date) ->
+    Date;
+to_erl_date(Date) when is_binary(Date) ->
+    <<DateB:19/binary, _/binary>>  = Date,
+    {ok, DateT} = tempo:parse(?FORMAT, {datetime, DateB}),
+    DateT;
+to_erl_date([A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16,A17,A18,A19|_]) ->
+    DateB = list_to_binary([A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16,A17,A18,A19]),
+    {ok, DateT} = tempo:parse(?FORMAT, {datetime, DateB}),
+    DateT;
+to_erl_date(Date) ->
+    throw({error, {wrong_date_fromat, Date}}).
+    
+
+    
+
+
+
+
+
+
+
 
 
 
